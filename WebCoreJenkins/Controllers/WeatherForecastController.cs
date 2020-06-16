aab5dcbf77b863339445f552e2f4cc8be2c35805
Tonatiuh <atk@hotmail.com>
2020-06-16 13:09:02 -0500

    Update WeatherForecastController.cs
    comentario solo a rama

--- WebCoreJenkins/Controllers/WeatherForecastController.cs
+++ WebCoreJenkins/Controllers/WeatherForecastController.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.Logging;
 
 namespace WebCoreJenkins.Controllers
 {
+    //Commit a Rama
     [ApiController]
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
